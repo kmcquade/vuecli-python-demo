2980d1b4f7fb5027f667811e3db35e96f76aa1d3
Kinnaird McQuade <kmcquade@salesforce.com>
2020-09-07 14:24:04 -0400

    Figured out structure that will work templating-wise

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,44 @@
-// vue.config.js
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-// var path = require('path');
+const path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: 'public/index.html',  //template file to embed the source
+//         inlineSource: '.(js|css)$' // embed all javascript and css inline
+//       }),
+//       new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+//     ]
+//   }
+// }
 module.exports = {
   // options...
-    indexPath: 'index.html',
+  //   indexPath: 'index.html',
     publicPath: '',
-    // configureWebpack: {
-    //     output: {
-    //         path: path.resolve(__dirname, './dist'),
-    //         filename: 'index_bundle.js'
-    //     },
-    //     plugins: [new HtmlWebpackPlugin()]
-    // },
+    configureWebpack: {
+        output: {
+            filename: '[name].bundle.js',
+            // filename: '[name].[contenthash].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new HtmlWebpackInlineSourcePlugin(),
+            new HtmlWebpackPlugin({
+                inject: true,
+                template: 'public/index.html',  //template file to embed the source
+                inlineSource: '.(js|css)$', // embed all javascript and css inline
+                title: 'Cloudsplaining report',
+            }),
+        ],
+        optimization: {
+            splitChunks: {
+                name: "vendor",
+                chunks: 'async',
+                hidePathInfo: true
+            }
+        }
+    },
     css: { extract: false }
 }
